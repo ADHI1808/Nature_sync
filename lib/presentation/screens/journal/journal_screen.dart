e1d7feab2987c3ca7c166861015f07d725e5515a
ASUS <s.adhithyan2004@gmail.com>
2024-08-09 21:10:01 +0530

    1st push

--- lib/presentation/screens/journal/journal_screen.dart
+++ lib/presentation/screens/journal/journal_screen.dart
@@ -6,7 +6,6 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:my_tflit_app/presentation/screens/journal/providers/state_providers.dart';
 
-
 import '../../../data/models/themes_model.dart';
 import '../../../data/providers/theme_provider.dart';
 import '../../../logic/localization/localization_handler.dart';
@@ -47,116 +46,22 @@ class JournalScreen extends HookConsumerWidget {
         ),
       ),
       backgroundColor: theme.backgroundColor,
-      body: PopScope(
-        onPopInvoked: (didPop) {
-          if (didPop) {
-            ref.read(inSavingStateProvider.notifier).state = false;
-            //cancelTimer.cancel();
-          }
-        },
-        child: SafeArea(
+      body: SafeArea(
+        child: Center(
           child: SingleChildScrollView(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10.0),
               child: Column(
                 children: [
+                  // Your code to make the screen empty
                   const SizedBox(height: 16),
-
-                  // Title
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        localizationHandler.getMessage(ref, "journal_title"),
-                        style: GoogleFonts.openSans(
-                          color: theme.textColor,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: isSaving
-                            ? () {}
-                            : () {
-                          settingsHandler.setValue(
-                              "journal_value", journalController.text);
-                          showElevatedNotification(
-                              context,
-                              localizationHandler.getMessage(
-                                  ref, "journal_update_successful"),
-                              theme.primaryColor);
-
-                          ref.read(inSavingStateProvider.notifier).state =
-                          true;
-
-                          Timer(const Duration(seconds: 5), () {
-                            if (context.mounted) {
-                              ref
-                                  .read(inSavingStateProvider.notifier)
-                                  .state = false;
-                            }
-                          });
-                        },
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.all(12),
-                          backgroundColor: isSaving
-                              ? theme.primaryColor
-                              : theme.secondaryColor,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16)),
-                        ),
-                        child: Icon(
-                          isSaving ? Icons.check : Icons.save,
-                          color: theme.textColor,
-                          size: 24,
-                        ),
-                      )
-                    ],
-                  ),
-
-                  const SizedBox(height: 16),
-
-                  // Journal Box
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border(
-                        left: BorderSide(
-                          color: theme.primaryColor,
-                          width: 2.0,
-                        ),
-                      ),
-                      borderRadius: BorderRadius.circular(4.0),
-                    ),
-                    child: TextField(
-                      controller: journalController,
-                      maxLines: MediaQuery.of(context).size.height ~/ 40,
-                      style: TextStyle(color: theme.textColor),
-                      decoration: InputDecoration(
-                        hintText: localizationHandler.getMessage(
-                            ref, "journal_take_notes"),
-                        hintStyle: TextStyle(color: theme.textColor),
-                        contentPadding: const EdgeInsets.all(8.0),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(4.0),
-                        ),
-                        focusedBorder: InputBorder.none,
-                        fillColor: theme.secondaryColor,
-                        filled: true,
-                      ),
-                      cursorColor: theme.primaryColor,
-                      textAlignVertical: TextAlignVertical.top,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      localizationHandler.getMessage(ref, "journal_disclaimer"),
-                      style: GoogleFonts.openSans(
-                        color: theme.textColor,
-                        fontSize: 14,
-                      ),
-                      textAlign: TextAlign.left,
+                  // Optionally, add a placeholder or a message
+                  Text(
+                    "No content available",
+                    style: GoogleFonts.openSans(
+                      color: theme.textColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ],
